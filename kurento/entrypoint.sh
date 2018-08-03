0d06beb73d39b4b41b72c3327b05b0fb211cb3d4
long0419 <borrip0419@gmail.com>
2018-08-03 18:12:16 +0800

    Revert "u"
    This reverts commit 4f7d23fa2e79dfa76ad2ee1d30f7b0a200df18ee.

--- kurento/entrypoint.sh
+++ kurento/entrypoint.sh
@@ -14,6 +14,4 @@ fi
 # Remove ipv6 local loop until ipv6 is supported
 cat /etc/hosts | sed '/::1/d' | tee /etc/hosts > /dev/null
 
-ln -s /dev/null /dev/raw1394
-
 exec /usr/bin/kurento-media-server "$@"
